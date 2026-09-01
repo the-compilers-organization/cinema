87a5695c77dba3cf28ec7e90456b6c818eacea3e
m4nd1skx <amandaaziz103@gmail.com>
2026-09-01 20:57:03 -0300

    Classe filme iniciada e testando na main

--- src/Filme.java
+++ src/Filme.java
@@ -1,3 +1,42 @@
 public class Filme {
+    private String nome;
+    private int duracao;
+    private String sinopse;
+    private double valor;
+
+    // CONSTRUTOR
+    public Filme(String nome, int duracao,String sinopse, double valor){
+        this.nome = nome;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+        this.valor = valor;
+    }
+
+    // GETS E SETS
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getDuracao() {
+        return duracao;
+    }
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+    public String getSinopse() {
+        return sinopse;
+    }
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
     
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-public class Main {
+public class Main {     //Testando FILME
     
     public static void main(String[] args){
-        
+        Filme filme1 = new Filme("\nFrozen", 100, "Gosto de abracos quentihos <3", 20.00);
+
+        System.out.println("Nome: " + filme1.getNome());
+        System.out.println("Duracao: " + filme1.getDuracao());
+        System.out.println("Sinopse: " + filme1.getSinopse());
+        System.out.println("Valor: R$" + filme1.getValor());
+
+        filme1.setValor(25.00);
+        System.out.println("Novo valor: "+filme1.getValor());
     }
 }
