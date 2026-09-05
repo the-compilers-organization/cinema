c08b89dd6a3e5a4546a3858aa6be626f676e5726
Sofiafs <sofiasantosf011@gmail.com>
2026-09-05 13:45:56 -0300

    Implementa classe Usuario com atributos, construtor e gets/sets

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,3 +1,97 @@
 public class Usuario {
-    
+    private String user;
+    private String cpf;
+    private String senha;
+    private int idade;
+    private String sexo;
+    private String email;
+    private String nomeCartao;
+    private String numeroCartao;
+    private String codigoVerificador;
+
+    // Construtor
+    public Usuario(String user, String cpf, String senha, int idade, String sexo, String email, String nomeCartao, String numeroCartao, String codigoVerificador){
+        this.user = user;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.email = email;
+        this.nomeCartao = nomeCartao;
+        this.numeroCartao = numeroCartao;
+        this.codigoVerificador = codigoVerificador;
+    }
+
+    // Gets e Sets
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNomeCartao() {
+        return nomeCartao;
+    }
+
+    public void setNomeCartao(String nomeCartao) {
+        this.nomeCartao = nomeCartao;
+    }
+
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+
+    public void setNumeroCartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+
+    public String getCodigoVerificador() {
+        return codigoVerificador;
+    }
+
+    public void setCodigoVerificador(String codigoVerificador) {
+        this.codigoVerificador = codigoVerificador;
+    }
 }
